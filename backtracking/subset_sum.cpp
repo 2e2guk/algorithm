bbf322b9b5b9f36d90a989209703a67d59747c50
이강욱 <90788409+2e2guk@users.noreply.github.com>
2023-02-21 19:37:01 +0900

    Update subset_sum.cpp

--- backtracking/subset_sum.cpp
+++ backtracking/subset_sum.cpp
@@ -10,7 +10,7 @@ bool promising(int i, int weight, int total, int W, vector<int>& w) {
     return (weight + total >= W) && (weight == W || weight + w[i + 1] <= W);
 }
 
-void sum_of_subsets(int i, int weight, int total, int W, vector<bool>& is_included, vector<int>& w) {
+void sum_of_subsets(int i, int weight, int total, int W, vector<bool>& is_included, vector<int>& w) { // i = 현재 level
     if (promising(i, weight, total, W, w)) {
         if (weight == W) {
             for (int j = 1; j <= i; j++) {
@@ -37,7 +37,7 @@ int main() {
         cin >> w[k];
     }
     total = accumulate(w.begin() + 1, w.end(), 0); 
-    sort(w.begin(), w.end());
-    sum_of_subsets(0, weight, total, W, is_included, w);
+    sort(w.begin() + 1, w.end());
+    sum_of_subsets(0, weight, total, W, is_included, w); // 시작 level은 0이다.
     return 0;
 }
